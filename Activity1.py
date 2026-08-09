254fa07b5bbba420efe5ca73b59efcbd764ba4f1
Yusuf Salim <yusuf.salim.atta@gmail.com>
2026-08-09 18:44:17 +0300

    class2

---
+++ Activity1.py
@@ -0,0 +1,9 @@
+print("Welcome to The World Of programming")
+
+print("4")
+
+print("Hello Python \n")
+
+print("Hello,5")
+
+print("Welcome To " , end="*")
---
+++ Activity2.py
@@ -0,0 +1,14 @@
+x = 5
+
+y = "John"
+
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+
+name =  input(" enter your  name:")
+
+print("\nHello" , name , "\nwelcome To codingal")
