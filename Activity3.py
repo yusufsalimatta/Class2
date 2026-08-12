531773e18020d3fb5fbbf005bab29e4d2d1c8761
Yusuf Salim <yusuf.salim.atta@gmail.com>
2026-08-12 19:06:31 +0300

    class2

--- Activity3.py
+++ Activity3.py
@@ -1,5 +1,15 @@
-import keyword 
+Yusuf = (Oct 4 2013)
 
+Mohammad = (Jan 1 2014)
 
-print(" Python keywords are . . .\n")
-print(keyword .kwlist)
+Ahmed = (Jun 18 2012)
+
+Salim = (Dec 25 2013)
+
+Ali = (Mar 6 2011)
+
+print("yusuf's Birthday  is on" , Yusuf)
+print("mohammad's Birthday  is on" , Mohammad)
+print("ahmed's Birthday  is on" , Ahmed)
+print("salim's Birthday  is on" , Salim)
+print("ali's Birthday  is on" , Ali)
